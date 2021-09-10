9720fc4a1dea899ffb19d95f7a3402b87ddd22b0
Pablo CE <pablocarrera@cye-atlantis.local>
2021-09-09 20:17:52 -0500

    refactor: 💡 autofocus search input

--- src/Search/component.tsx
+++ src/Search/component.tsx
@@ -18,7 +18,7 @@ const Search: React.FC = () => {
   return (
     <section className="app-search pb-6 border-bottom">
         <img className="search-icon" src={process.env.PUBLIC_URL + '/assets/img/shuttle.svg'} width="45"/>
-        <input className="form-control input-block input-dark" value={search} onChange={handleChange} onKeyDown={handleKeyDown} type="text" placeholder="" aria-label="Full-width input" />
+        <input className="form-control input-block input-dark" value={search} onChange={handleChange} onKeyDown={handleKeyDown} type="text" placeholder="" aria-label="Full-width input" autofocus/>
         <div className="d-flex flex-justify-center mt-6">
         <a href={'https://www.google.com/search?safe=strict&q=' + search} className="btn btn-outline mr-3 box-shadow-none pt-5 pb-5 pl-7 pr-7" type="button">Google Search</a>
         <a href={'https://duckduckgo.com/?q=' + search} className="btn btn-outline mr-3 box-shadow-none pt-5 pb-5 pl-7 pr-7" type="button">DuckDuckGo Search</a>
