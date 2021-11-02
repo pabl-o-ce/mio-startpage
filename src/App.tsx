943bbc3a8efe60a0a183b1fac6999980ed282aa6
Pablo CE <pablocarrera@cye-atlantis.local>
2021-11-02 16:25:29 -0500

    refactor: 💡 change title

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ const App = () : JSX.Element => {
       <Header/>
       <Search/>
       <Bookmark/>
-      <img className="position-absolute top-0 left-0fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -10}} width="50%"/>
-      <img className="position-absolute right-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -10}} width="50%"/>
-      <img className="position-absolute top-0 left-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -10}} width="50%"/>
-      <img className="position-absolute right-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -10}} width="50%"/>
+      <img className="position-absolute top-0 left-0 right-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -10}} width="50%"/>
+      <img className="position-absolute top-0 right-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -10}} width="50%"/>
+      <img className="position-absolute top-0 left-0 right-0 bottom-0 rotate" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -10}} width="50%"/>
+      <img className="position-absolute top-0 right-0 bottom-0 rotate" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -10}} width="50%"/>
     </div>
   );
 }
