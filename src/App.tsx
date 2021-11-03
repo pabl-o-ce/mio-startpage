c21a89763534cf217a3609c1e099e58c2496b041
Pablo CE <pablocarrera@cye-atlantis.local>
2021-11-02 23:17:22 -0500

    fix: 🐛 starts

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import './App.scss';
 import { Header } from './Header';
 import { Search } from './Search';
 import { Bookmark } from './Bookmark';
+import { Fragment } from 'react';
 
 const App = () : JSX.Element => {
   return (
-    <div className="App">
-      <Header/>
-      <Search/>
-      <Bookmark/>
-      <img className="position-absolute top-0 left-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -1}} width="50%"/>
+    <Fragment>
+      <div className="App">
+        <Header/>
+        <Search/>
+        <Bookmark/>
+      </div>
+      {/* <img className="position-absolute top-0 left-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -1}} width="50%"/>
       <img className="position-absolute right-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -1}} width="50%"/>
       <img className="position-absolute left-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -1}} width="50%"/>
-      <img className="position-absolute top-0 right-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -1}} width="50%"/>
-    </div>
+      <img className="position-absolute top-0 right-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -1}} width="50%"/> */}
+    </Fragment>
   );
 }
 
