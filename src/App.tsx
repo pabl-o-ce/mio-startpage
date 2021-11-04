3efa45b71b7cbec5a41c38830cd4eb5dc8a83f34
Pablo CE <pablocarrera@cye-atlantis.local>
2021-11-03 23:56:10 -0500

    fix: 🐛 fix stars animation an scroll

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ const App = () : JSX.Element => {
         <Header/>
         <Search/>
         <Bookmark/>
+        <img className="position-absolute top-0 left-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
+        <img className="position-absolute right-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
+        <img className="position-absolute left-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
+        <img className="position-absolute top-0 right-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
       </div>
-      {/* <img className="position-absolute top-0 left-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -1}} width="50%"/>
-      <img className="position-absolute right-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -1}} width="50%"/>
-      <img className="position-absolute left-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -1}} width="50%"/>
-      <img className="position-absolute top-0 right-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: -1}} width="50%"/> */}
     </Fragment>
   );
 }
