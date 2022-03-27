8e8c5a3a67a3dad70c4602bc8fc173127a9a436c
Pablo CE <pablocarrera@cye-atlantis.local>
2022-03-27 12:25:28 -0500

    feat: 🎸 duckduckgo only support [just use ddg search features

--- src/Search/component.tsx
+++ src/Search/component.tsx
@@ -13,20 +13,20 @@ const Search = (): JSX.Element => {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     switch (e.key) {
       case 'Enter':
-        window.location.href = `https://www.google.com/search?safe=strict&q=${search}`;
-        break;
-      case 'ArrowUp':
         window.location.href = `https://www.duckduckgo.com/?q=${search}`;
         break;
-      case 'ArrowDown':
-        window.location.href = `https://www.amazon.com/s?k=${search}`;
-        break;
-      case 'ArrowRight':
-        window.location.href = `https://www.newegg.com/p/pl?d=${search}`;
-        break;
-      case 'ArrowLeft':
-        window.location.href = `https://github.com/search?q=${search}`;
-        break;
+      // case 'ArrowUp':
+      //   window.location.href = `https://www.duckduckgo.com/?q=${search}`;
+      //   break;
+      // case 'ArrowDown':
+      //   window.location.href = `https://www.amazon.com/s?k=${search}`;
+      //   break;
+      // case 'ArrowRight':
+      //   window.location.href = `https://www.newegg.com/p/pl?d=${search}`;
+      //   break;
+      // case 'ArrowLeft':
+      //   window.location.href = `https://github.com/search?q=${search}`;
+      //   break;
       default: 
         break;
     }
@@ -35,7 +35,7 @@ const Search = (): JSX.Element => {
     <section className="app-search pb-6 border-bottom">
         <img className="search-icon" src={process.env.PUBLIC_URL + '/assets/img/shuttle.svg'} width="45"/>
         <input className="form-control input-block input-dark" value={search} onChange={handleChange} onKeyDown={handleKeyDown} type="text" placeholder="" aria-label="Full-width input" autoFocus/>
-        <div className="d-flex flex-justify-center mt-6">
+        {/* <div className="d-flex flex-justify-center mt-6">
           <a href={`https://www.google.com/search?safe=strict&q=${search}`} className="btn box-shadow-none" type="button">
             <img className="search-google" src={`${process.env.PUBLIC_URL}/assets/img/google.svg`} width="22"/>
           </a>
@@ -45,7 +45,7 @@ const Search = (): JSX.Element => {
           <a href={`https://www.amazon.com/s?k=${search}`} className="btn box-shadow-none" type="button">
             <img className="search-amazon" src={`${process.env.PUBLIC_URL}/assets/img/amazon.svg`} width="25"/>
           </a>
-        </div>
+        </div> */}
     </section>
   )};
 
