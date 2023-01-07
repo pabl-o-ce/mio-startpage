25feae5d05af7e03e0edf9df72514f143ca97b95
pabl-o-ce <cye@poscye.com>
2023-01-06 20:33:59 -0500

    fix: 🐛 animation

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ import { Fragment } from 'react';
 const App = () : JSX.Element => {
   return (
     <Fragment>
-      <div className="App">
+      <div className="App relative overflow-hidden">
         <Header/>
         <Search/>
         <Bookmark/>
-        <img className="position-absolute top-0 left-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
-        <img className="position-absolute right-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
-        <img className="position-absolute left-0 bottom-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
-        <img className="position-absolute top-0 right-0 fadeOut" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
+        <img className="absolute top-0 left-0 animate-pulse rotate" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
+        <img className="absolute right-0 bottom-0 animate-pulse rotate" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
+        <img className="absolute left-0 bottom-0 animate-pulse rotate0" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
+        <img className="absolute top-0 right-0 animate-pulse rotate" src={process.env.PUBLIC_URL + "/assets/img/stars.svg"} alt="" style={{zIndex: 1}} width="50%"/>
       </div>
     </Fragment>
   );
