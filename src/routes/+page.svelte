b24f1bee4521237ebc5dde6acf4e41b3107327f7
pabl-o-ce <pabloce@poscye.com>
2025-02-26 23:46:08 -0500

    fix: bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
     <Search/>
     <Bookmarks/>
     <div class="absolute inset-0">
-      <div class="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_50%)]"></div>
+      <div class="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#6633ee36_100%)]"></div>
     </div>
     <img class="absolute top-0 left-0 animate-pulse rotate" src={"/img/stars.svg"} alt="" width="50%"/>
     <img class="absolute right-0 bottom-0 animate-pulse rotate" src={"/img/stars.svg"} alt="" width="50%"/>
